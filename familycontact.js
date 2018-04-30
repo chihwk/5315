787e18d60117bec107d1cf4b0de2a3efb885ac9f
donna <kuangc@seattleu.edu>
2018-04-29 21:38:30 -0700

    familycontact.html

---
+++ familycontact.js
@@ -0,0 +1,33 @@
+{
+  "data": [
+    {
+      "name": "KSF",
+      "who": "my dad",
+      "age": "55",
+      "base": "Beijing",
+      "phone": "+86 1391000000"
+    },
+    {
+      "name": "LYH",
+      "who": "my mom",
+      "age": "53",
+      "base": "Beijing, Taiwan",
+      "phone": "+886 911111111"
+    },
+    {
+      "name": "KCJ",
+      "who": "my sister",
+      "age": "24",
+      "base": "Beijing, Taiwan, Seattle",
+      "phone": "+1 206 555 5555"
+    },
+    {
+      "name": "LHY",
+      "who": "Little sister",
+      "age": "5",
+      "base": "Shanghai",
+      "phone": "N/A"
+    }
+    
+  ]
+}
